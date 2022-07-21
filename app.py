61fdae5e9e38f76e4716974eb7eb346dba8d356c
silver-or <laura817@naver.com>
2022-07-21 17:53:59 +0900

    2022-07-21 17:53 FastAPI

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
+import os
+
+UPLOAD_FOLDER = './save/'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def render_file():
@@ -11,7 +15,8 @@ def render_file():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return 'file uploaded successfully'
     else:
         return render_file('upload.html')
